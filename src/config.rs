83e52c3517770a9eb140d6f2ab1c624ef2a7bcb3
Shiroko3 <mhy1630005@163.com>
2025-05-12 19:28:40 +0800

    update

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["link-sp-1.ru2023.top"];
+pub const RS_PUB_KEY: &str = "M1gwniOV42x+GaBuhoRxOssYKl0x3RGUicK+JUVLHgE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
